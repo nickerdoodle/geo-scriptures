32bf9e93b95351057babe6145ac8acbe90e7a6a6
Nick <nicholasmahe@gmail.com>
2019-11-02 15:45:24 -0600

    Initial Commit

---
+++ Project 3 Version 1/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Project 3 Version 1
+//
+//  Created by Nick Mahe on 11/2/19.
+//  Copyright © 2019 Nick Mahe. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
